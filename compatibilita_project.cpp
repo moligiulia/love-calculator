66ffb1a924fb01d0ccabd6243e7ba551f7737c76
moligiulia <molinari2004@gmail.com>
2026-02-12 10:57:00 +0100

    Update compatibilita_project.cpp

--- compatibilita_project.cpp
+++ compatibilita_project.cpp
@@ -1,15 +1,15 @@
 ﻿#include <drogon/drogon.h>
-#include <iostream>  // <--- Fondamentale per risolvere l'errore su cout e endl
+#include <iostream>
 #include "controllers/CalcolatoreController.h"
 
 int main() {
-    // Risolviamo l'errore C2653: ora il compilatore sa cos'è 'drogon'
-    drogon::app().addListener("127.0.0.1", 8080);
+    // CORRETTO: 0.0.0.0 permette a Render di vedere il server
+    drogon::app().addListener("0.0.0.0", 8080);
 
     drogon::app().setThreadNum(4);
 
-    // Risolviamo l'errore C2039: std::cout ora è riconosciuto grazie a <iostream>
-    std::cout << "DEBUG: Server in fase di avvio su http://127.0.0.1:8080" << std::endl;
+    // AGGIORNATO: Cambiamo anche qui per coerenza con i log di Render
+    std::cout << "DEBUG: Server in fase di avvio su http://0.0.0.0:8080" << std::endl;
 
     drogon::app().run();
     return 0;
